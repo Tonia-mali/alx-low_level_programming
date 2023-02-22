28ff48d7153ef3c3a68d386b99f66dc48813d238
Tonia-mali <wawiratonnia@gmail.com>
2023-02-22 15:29:02 -0800

    mainh

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,59 +1,59 @@
 /**
  * _putchar - Prints putchar
  */
-putchar();
+int _putchar(char c);
 
 /**
  * print_alphabet - prints the alphabet in lowercase
  */
-print_alphabet(void);
+void print_alphabet(void);
 
 /**
  * print_alphabet_x10 - prints 10 times the alphabet
  */
-print_alphabet_x10(void);
+void print_alphabet_x10(void);
 
 /**
- * _islower -  checks for lowercase character
+ * _islower - checks for lowercase character
  */
-_islower(int c);
+int _islower(int c);
 
 /**
  * _isalpha - checks for alphabetic character
  */
-_isalpha(int c);
+int _isalpha(int c);
 
 /**
  * print_sign - prints the sign of a number
  */
-print_sign(int n);
+int print_sign(int n);
 
 /**
  * _abs - computes absolute value of an integer
  */
-_abs(int);
+int _abs(int);
 
 /**
  * print_last_digit - prints the last digit of a number
  */
-print_last_digit(int);
+int print_last_digit(int);
 
 /**
  * jack_bauer - prints every minute of Jack Bauer's day
  */
-jack_bauer(void);
+void jack_bauer(void);
 
 /**
  * times_table - prints the 9 times table
  */
-times_table(void);
+void times_table(void);
 
 /**
  * add - adds two integers
  */
-add(int, int);
+int add(int, int);
 
 /**
  * print_to_98 - prints all natural numbers from n to 98
  */
-print_to_98(int n);
+void print_to_98(int n);
