365de9caa138984876e07af1672982b5291f0dbe
Tonia-mali <wawiratonnia@gmail.com>
2023-02-22 13:57:20 -0800

    prototypes

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,59 @@
+/**
+ * _putchar - Prints putchar
+ */
+putchar();
+
+/**
+ * print_alphabet - prints the alphabet in lowercase
+ */
+print_alphabet(void);
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet
+ */
+print_alphabet_x10(void);
+
+/**
+ * _islower -  checks for lowercase character
+ */
+_islower(int c);
+
+/**
+ * _isalpha - checks for alphabetic character
+ */
+_isalpha(int c);
+
+/**
+ * print_sign - prints the sign of a number
+ */
+print_sign(int n);
+
+/**
+ * _abs - computes absolute value of an integer
+ */
+_abs(int);
+
+/**
+ * print_last_digit - prints the last digit of a number
+ */
+print_last_digit(int);
+
+/**
+ * jack_bauer - prints every minute of Jack Bauer's day
+ */
+jack_bauer(void);
+
+/**
+ * times_table - prints the 9 times table
+ */
+times_table(void);
+
+/**
+ * add - adds two integers
+ */
+add(int, int);
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ */
+print_to_98(int n);
