ec051103b0fd0dc625dd0a8e6178c36756accdcb
Tonia-mali <wawiratonnia@gmail.com>
2023-02-23 05:50:36 -0800

    main.h

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,49 @@
+/**
+ * _isupper - checks for uppercase character
+ */
+int _isupper(int c);
+
+/**
+ * _isdigit - checks for a digit
+ */
+int _isdigit(int c);
+
+/**
+ * mul - function that multiplies two integers
+ */
+int mul(int a, int b);
+
+/**
+ * print_numbers - function that prints the numbers, from 0 to 9
+ */
+void print_numbers(void);
+
+/**
+ * print_most_numbers - function that prints the numbers, from 0 to 9
+ */
+void print_most_numbers(void);
+
+/**
+ * more_numbers - function that prints 10 times the numbers, from 0 to 14
+ */
+void more_numbers(void);
+
+/**
+ * print_line - function that draws a straight line in the terminal
+ */
+void print_line(int n);
+
+/**
+ * print_diagonal - function that draws a diagonal line on the terminal
+ */
+void print_diagonal(int n);
+
+/**
+ * print_square - function that prints a square, followed by a new line
+ */
+void print_square(int size);
+
+/**
+ * print_triangle - function that prints a triangle
+ */
+void print_triangle(int size);
