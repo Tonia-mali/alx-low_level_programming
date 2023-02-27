114e257d387bbd2e86bddb5a450ff908793743fe
Tonia-mali <wawiratonnia@gmail.com>
2023-02-27 06:07:02 -0800

    main.h

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,54 @@
+/**
+ * _putchar - writes the character c to stdout
+ */
+int _putchar(char c);
+
+/**
+ * reset_to_98 - function that takes a pointer to an int as parameter
+ */
+void reset_to_98(int *n);
+
+/**
+ * swap_int - function that swaps the values of two integers
+ */
+void swap_int(int *a, int *b);
+
+/**
+ * _strlen - function that returns the length of a string
+ */
+int _strlen(char *s);
+
+/**
+ * _puts - function that prints a string, followed by a new line, to stdout
+ */
+void _puts(char *str);
+
+/**
+ * print_rev - function that prints a string, in reverse
+ */
+void print_rev(char *s);
+
+/**
+ * rev_string - function that reverses a string
+ */
+void rev_string(char *s);
+
+/**
+ * puts2 - function that prints every other character of a string
+ */
+void puts2(char *str);
+
+/**
+ * puts_half - function that prints half of a string
+ */
+void puts_half(char *str);
+
+/**
+ * print_array - function that prints n elements of an array of integers
+ */
+void print_array(int *a, int n);
+
+/**
+ * *_strcpy - function that copies the string pointed to by src
+ */
+char *_strcpy(char *dest, char *src);
